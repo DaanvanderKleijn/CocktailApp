9810095993fe24666550ab3c80d6a80970ba074b
Daan <d251901@edu.rocwb.nl>
2018-11-06 21:56:19 +0100

    App

---
+++ CocktailApp/CocktailApp/CocktailApp/View/Instellingen.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CocktailApp.View
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Instellingen : ContentPage
+	{
+		public Instellingen ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
---
+++ CocktailApp/CocktailApp/CocktailApp/View/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CocktailApp.View
+{
+    public partial class MainPage : ContentPage
+    {
+        public ObservableCollection<string> mainMenuItemList { get; set; }
+        public MainPage()
+        {
+            InitializeComponent();
+            mainMenuItemList = new ObservableCollection<string>();
+
+            mainMenuItemList.Add("Instellingen");
+            mainMenuItemList.Add("Categorieën");
+            mainMenuItemList.Add("EULA");
+            mainMenuItemList.Add("Achievements");
+
+            mainMenu.ItemsSource = mainMenuItemList;
+        }
+
+        public void UpdateContentPage(object sender, Syncfusion.ListView.XForms.ItemAppearingEventArgs e)
+        {
+            switch (e.ItemData.ToString())
+            {
+                case "Instellingen":
+                    navDrawer.ContentView = new Instellingen().Content;
+                    break;
+
+                case "Categorieën":
+                    navDrawer.ContentView = new Categorieën().Content;
+                    break;
+
+                case "EULA":
+                    navDrawer.ContentView = new EULA().Content;
+                    break;
+
+                case "Achievements":
+                    navDrawer.ContentView = new Achievements().Content;
+                    break;
+            }
+            navDrawer.ToggleDrawer();
+        }
+    }
+}
